0d33eb466a1c51404a86fbb94a7f913cbe929137
Ye Yu <nearjilt@gmail.com>
2025-02-23 08:54:21 +0800

    feat: complete basic typing area layout

--- src/components/layout/header/home-header.tsx
+++ src/components/layout/header/home-header.tsx
@@ -2,7 +2,7 @@ import HomeMenuCard from './internal/home-menu-card'
 
 const HomeHeader = () => {
   return (
-    <nav className="h-16 flex items-center px-4 justify-between w-full bg-slate-900">
+    <nav className="h-16 flex items-center px-4 justify-between w-full border-b border-dashed">
       <h1 className="font-semibold text-lg text-pink-500 font-mono">
         Suki Typing
       </h1>
--- src/components/layout/typing-content/home-typing.tsx
+++ src/components/layout/typing-content/home-typing.tsx
@@ -1,7 +1,18 @@
 const HomeTyping = () => {
   return (
-    <main className="bg-slate-950 w-full h-full">
-      <div>测试</div>
+    <main className="bg-slate-800 w-full h-full flex flex-col border border-dashed items-center gap-16 py-12">
+      {/* 题目 */}
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, beatae.
+      </h3>
+      {/* 输入框 */}
+      <main>
+        <input type="text" />
+      </main>
+      {/* 键盘显示区域提示 */}
+      <section>
+        <div>键盘显示区域</div>
+      </section>
     </main>
   )
 }
