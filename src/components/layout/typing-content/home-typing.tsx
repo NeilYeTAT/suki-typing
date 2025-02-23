ad0245382950fd9ee4f58039b2e2f38852b7e5b6
Ye Yu <nearjilt@gmail.com>
2025-02-23 11:34:56 +0800

    feat: implement basic input and validation functionality

--- src/components/layout/typing-content/home-typing.tsx
+++ src/components/layout/typing-content/home-typing.tsx
@@ -1,13 +1,58 @@
+'use client'
+
+import dict from '@dict/jp-test.json'
+import { useState } from 'react'
+
+const questionsArray = dict
+
 const HomeTyping = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value !== ' ') {
+      setInputValue(e.target.value)
+    }
+  }
+
+  const submitAnswer = () => {
+    const ok = questionsArray[currentQuestionIndex].romaji === inputValue.trim()
+    if (ok) {
+      setCurrentQuestionIndex(pre => pre + 1)
+    }
+    setInputValue('')
+  }
+
+  const handleSubmitShortcut = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === ' ' || e.key === 'Enter') {
+      submitAnswer()
+    }
+  }
+
   return (
     <main className="bg-slate-800 w-full h-full flex flex-col border border-dashed items-center gap-16 py-12">
-      {/* 题目 */}
-      <h3>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, beatae.
-      </h3>
+      <h2 className="text-6xl border-b border-dashed border-white">
+        {currentQuestionIndex < questionsArray.length ? (
+          questionsArray[currentQuestionIndex].hiragana
+        ) : (
+          <p>Over~</p>
+        )}
+      </h2>
       {/* 输入框 */}
-      <main>
-        <input type="text" />
+      <main className="flex flex-col">
+        <section className="relative border-b border-dashed w-36 h-16">
+          <input
+            type="text"
+            value={inputValue}
+            className="appearance-none size-full text-3xl outline-none absolute top-0 left-0 px-2 bg-transparent border-b border-pink-300"
+            onChange={handleInputChange}
+            onKeyDown={handleSubmitShortcut}
+          />
+        </section>
+
+        <button onClick={() => setCurrentQuestionIndex(pre => pre + 1)}>
+          next
+        </button>
       </main>
       {/* 键盘显示区域提示 */}
       <section>
